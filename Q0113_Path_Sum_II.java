7f0eb114057b7baa7478bc5b75fa74436b9549fe
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-11 23:13:31 -0500

    Update Q0113_Path_Sum_II.java

--- Q0113_Path_Sum_II.java
+++ Q0113_Path_Sum_II.java
@@ -11,31 +11,23 @@ import java.util.List;
  * Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
  */
 public class Q113_Path_Sum_II {
-  List<List<Integer>> res = new ArrayList();
-  public List<List<Integer>> pathSum(TreeNode root, int sum) {
-    if (root == null) return res;   
-    List<Integer> path = new ArrayList();
-    path.add(root.val);
-    dfs(root, sum, path);
-    return res;
-  }
-  
-  private void dfs(TreeNode root, int sum, List<Integer> path) {
-    if (root.left == null && root.right == null && sum == root.val) {
-      res.add(new ArrayList(path));
-      return;
+    List<List<Integer>> res = new ArrayList();
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        helper(root, sum, new ArrayList());
+        return res;
     }
     
-    if (root.left != null) {
-      path.add(root.left.val);
-      dfs(root.left, sum-root.val, path);
-      path.remove(path.size()-1);
+    private void helper(TreeNode root, int sum, List<Integer> path) {
+        if (root == null) return;
+        path.add(root.val);
+        int len = path.size();
+        if (root.left == null && root.right == null && sum == root.val) {
+            res.add(new ArrayList(path));
+            path.remove(len-1);
+            return;
+        }
+        helper(root.left, sum-root.val, path);
+        helper(root.right, sum-root.val, path);
+        path.remove(len-1);
     }
-    
-    if (root.right != null) {
-      path.add(root.right.val);
-      dfs(root.right, sum-root.val, path);
-      path.remove(path.size()-1);
-    }
-  }
 }
