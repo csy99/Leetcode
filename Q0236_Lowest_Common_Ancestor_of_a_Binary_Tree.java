d0eb19750622ad22c081f05a8a8ebc6d174faabf
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-12 10:56:22 -0500

    Update Q0236_Lowest_Common_Ancestor_of_a_Binary_Tree.java

--- Q0236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
+++ Q0236_Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -6,20 +6,31 @@ import Leetcode.Util.TreeNode;
  * Created by csy99 on 4/6/20.
  */
 public class Q236_Lowest_Common_Ancestor_of_a_Binary_Tree {
-  TreeNode ancestor = null;
-  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-    dfs(root, p, q);
-    return ancestor;
-  }
+    // recursion
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null || right == null)
+            return left == null ? right : left;
+        return root;
+    }
   
-  private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
-    if (root == null) return false;
-    int left = dfs(root.left, p, q) ? 1 : 0;
-    int right = dfs(root.right, p, q) ? 1 : 0;
-    // check if root is either p or q
-    int mid = root == p || root == q ? 1 : 0;
-    if (left + mid + right > 1)
-      ancestor = root;
-    return left + mid + right > 0;
-  }
+    // dfs
+    TreeNode ancestor = null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+      dfs(root, p, q);
+      return ancestor;
+    }
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+      if (root == null) return false;
+      int left = dfs(root.left, p, q) ? 1 : 0;
+      int right = dfs(root.right, p, q) ? 1 : 0;
+      // check if root is either p or q
+      int mid = root == p || root == q ? 1 : 0;
+      if (left + mid + right > 1)
+        ancestor = root;
+      return left + mid + right > 0;
+    }
 }
