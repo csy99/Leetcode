67eca9702b9dab219c50e9313d8ffb4f2a76f89b
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-12 10:34:47 -0500

    Update Q0129_Sum_Root_to_Leaf_Numbers.java

--- Q0129_Sum_Root_to_Leaf_Numbers.java
+++ Q0129_Sum_Root_to_Leaf_Numbers.java
@@ -8,22 +8,22 @@ import Leetcode.Util.TreeNode;
  * https://leetcode.com/problems/sum-root-to-leaf-numbers/
  */
 public class Q129_Sum_Root_to_Leaf_Numbers {
-  int sum = 0;
-  public int sumNumbers(TreeNode root) {
-    helper(root, 0);
-    return sum;
-  }
-  
-  private void helper(TreeNode root, int cur) {
-    if (root == null) {
-      return;
+    int sum = 0;
+    public int sumNumbers(TreeNode root) {
+      helper(root, 0);
+      return sum;
     }
-    if (root.left == null && root.right == null) {
-      sum += cur * 10 + root.val;
-      return;
+
+    private void helper(TreeNode root, int cur) {
+      if (root == null) {
+        return;
+      }
+      if (root.left == null && root.right == null) {
+        sum += cur * 10 + root.val;
+        return;
+      }
+      cur = cur * 10 + root.val;
+      helper(root.left, cur);
+      helper(root.right, cur);
     }
-    cur = cur * 10 + root.val;
-    helper(root.left, cur);
-    helper(root.right, cur);
-  }
 }
