d8aabbb4c62c7a0e90cabfe366dcd8e7ba8ea212
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-11 23:19:22 -0500

    Update Q437_Path_Sum_III.java

--- Q437_Path_Sum_III.java
+++ Q437_Path_Sum_III.java
@@ -5,26 +5,24 @@ import Leetcode.Util.TreeNode;
 import java.util.HashMap;
 
 /**
- * Created by rbhatnagar2 on 1/15/17.
+ * Created by csy99 on 5/11/20.
  */
 public class Q437_Path_Sum_III {
     public int pathSum(TreeNode root, int sum) {
-        HashMap<Integer, Integer> preSum = new HashMap();
-        preSum.put(0, 1);
-        return helper(root, 0, sum, preSum);
-    }
-
-    public int helper(TreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
-        if (root == null) {
+        if (root == null)
             return 0;
-        }
-
-        currSum += root.val;
-        int res = preSum.getOrDefault(currSum - target, 0);
-        preSum.put(currSum, preSum.getOrDefault(currSum, 0) + 1);
-
-        res += helper(root.left, currSum, target, preSum) + helper(root.right, currSum, target, preSum);
-        preSum.put(currSum, preSum.get(currSum) - 1);
-        return res;
+        return count(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    }
+    
+    private int count(TreeNode root, int sum) {
+        if (root == null)
+            return 0;
+        sum -= root.val;
+        int cnt = 0;
+        if (sum == 0)
+            cnt += 1;
+        int left = count(root.left, sum);
+        int right = count(root.right, sum);
+        return cnt + left + right;
     }
 }
