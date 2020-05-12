f88834d84fda508c445c47d9d48c953b772b6b92
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-11 20:09:04 -0500

    Update Q0110_Balanced_Binary_Tree.java

--- Q0110_Balanced_Binary_Tree.java
+++ Q0110_Balanced_Binary_Tree.java
@@ -6,19 +6,30 @@ import Leetcode.Util.TreeNode;
  * Created by csy99 on 4/4/20.
  */
 public class Q110_Balanced_Binary_Tree {
-  public boolean isBalanced(TreeNode root) {
-    if (height(root, 0) == -1)
-      return false;
-    return true;
-  }
-  
-  private int height(TreeNode root, int depth) {
-    if (depth == -1) return -1;
-    if (root == null) return depth;
-    int left = height(root.left, depth + 1);
-    int right = height(root.right, depth + 1);
-    if (Math.abs(left - right) > 1)
-      return -1;
-    return Math.max(left, right);
-  }
+    // HashMap<TreeNode, Integer> h = new HashMap();
+    public boolean isBalanced(TreeNode root) {
+        if (height(root) < 0)
+            return false;
+        return true;
+    }
+    
+    private int height(TreeNode root) {
+        if (root == null)
+            return 0;
+        // if (h.containsKey(root))
+        //     return h.get(root);
+        if (root.left == null && root.right == null) {
+            // h.put(root, 1);
+            return 1;
+        }
+        int leftHi = height(root.left);
+        int rightHi = height(root.right);
+        if (leftHi == -1 || rightHi == -1)
+            return -1;
+        if (Math.abs(leftHi - rightHi) > 1) 
+            return -1;
+        int curHi = 1 + Math.max(leftHi, rightHi);
+        // h.put(root, curHi);
+        return curHi;
+    }
 }
