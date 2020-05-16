8ba03b8ab12938e176c507de22479083870507f2
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-16 16:14:39 -0500

    Update Q0162_Find_Peak_Element.java

--- Q0162_Find_Peak_Element.java
+++ Q0162_Find_Peak_Element.java
@@ -9,7 +9,7 @@ public class Q0162_Find_Peak_Element {
             int r = nums.length - 1;
             while (l < r) {
                 int mid = l + (r-l)/2;
-                if (nums[mid] <= nums[mid+1])
+                if (nums[mid] < nums[mid+1])
                     l = mid+1;
                 else
                     r = mid;
