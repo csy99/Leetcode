68ca4fc5d85ef224d75f128738d73234a9f23b97
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-16 16:13:08 -0500

    Update Q0162_Find_Peak_Element.java

--- Q0162_Find_Peak_Element.java
+++ Q0162_Find_Peak_Element.java
@@ -4,17 +4,16 @@ package Leetcode;
  * Created by csy99 on 3/16/20.
  */
 public class Q0162_Find_Peak_Element {
-    public int findPeakElement(int[] nums) {
-        if (nums == null || nums.length == 1)
-            return 0;
-        else if (nums.length == 2) {
-            return (nums[0] > nums[1]) ? 0 : 1;
-        }
-        for (int i = 1; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i - 1] && nums[i] > nums[i + 1])
-                return i;
-        }
-        // if no peak is found, then arr is sorted (asc or desc), return highest
-        return (nums[0] > nums[nums.length - 1]) ? 0 : nums.length - 1;
-    }
+      public int findPeakElement(int[] nums) {
+            int l = 0;
+            int r = nums.length - 1;
+            while (l < r) {
+                int mid = l + (r-l)/2;
+                if (nums[mid] <= nums[mid+1])
+                    l = mid+1;
+                else
+                    r = mid;
+            }
+            return l;
+      }
 }
