32004305c30f35c595151af6b3ea83163516a3d4
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-17 15:22:48 -0500

    Update Q0141_Linked_List_Cycle.java

--- Q0141_Linked_List_Cycle.java
+++ Q0141_Linked_List_Cycle.java
@@ -6,17 +6,31 @@ import Leetcode.Util.ListNode;
  * Created by csy99 on 3/17/20.
  */
 public class Q0141_Linked_List_Cycle {
-  public boolean hasCycle(ListNode head) {
-    if (head == null || head.next == null)  // no cycle for sure
-      return false; 
-    ListNode fast = head.next;
-    ListNode slow = head;
-    while (slow != fast) {
-      if (fast == null || fast.next == null)
-        return false;
-      fast = fast.next.next;
-      slow = slow.next;
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null)  // no cycle for sure
+            return false; 
+        ListNode fast = head.next;
+        ListNode slow = head;
+        while (slow != fast) {
+            if (fast == null || fast.next == null)
+                return false;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
     }
-    return true;
-  }
+    
+    public boolean hasCycle(ListNode head) {
+        if (head == null)
+            return false;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
 }
