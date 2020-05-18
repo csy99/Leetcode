dcb8d8430691789a6474cf3e3a10b5d79bed09fc
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-18 14:22:10 -0500

    Update Q0098_Validate_Binary_Search_Tree.java

--- Q0098_Validate_Binary_Search_Tree.java
+++ Q0098_Validate_Binary_Search_Tree.java
@@ -16,3 +16,33 @@ public class Q098_Validate_Binary_Search_Tree {
     return helper(root.left, min, root.val) && helper(root.right, root.val, max);
   }
 }
+
+// a little faster
+class Solution {
+    boolean valid = true;
+    public boolean isValidBST(TreeNode root) {
+        range(root);
+        return valid;
+    }
+    
+    private int[] range(TreeNode root) {
+        if (!valid) return null;
+        if (root == null) return null;
+        int[] range = new int[] {root.val, root.val};
+        if (root.left == null && root.right == null)
+            return range;
+        int[] left = range(root.left);
+        int[] right = range(root.right);
+        if (left != null) {
+            range[0] = left[0];
+            if (root.val <= left[1])
+                valid = false;
+        }
+        if (right != null) {
+            range[1] = right[1];
+            if (root.val >= right[0])
+                valid = false;
+        }
+        return range;
+    }
+}
