972e1bf39829c82c9ac01489116a3e250ffa6369
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-19 12:47:26 -0500

    Update Q0015_3Sum.java

--- Q0015_3Sum.java
+++ Q0015_3Sum.java
@@ -11,44 +11,35 @@ import java.util.Set;
  */
 public class Q015_3Sum {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums == null || nums.length <= 2)
-            return res;
         Arrays.sort(nums);
-        int i = 0; 
-        while (i + 2 < nums.length) {
-            int base = nums[i];
-            int left = i + 1;
-            int right = nums.length - 1;
-            while (left < right) {
-                int sum = base + nums[left] + nums[right];
+        ArrayList<List<Integer>> zero = new ArrayList();
+        for (int i = 0; i < nums.length-2; i++) {
+            if (nums[i] > 0)  // pruning
+                break;
+            if (i > 0 && nums[i] == nums[i-1])
+                continue;
+            int l = i+1;
+            int r = nums.length-1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
                 if (sum == 0) {
-                    List<Integer> sol = new ArrayList();
-                    sol.add(base);
-                    sol.add(nums[left]);
-                    sol.add(nums[right]);
-                    res.add(sol);
-                    left = moveLeft(nums, left+1);
-                    right = moveRight(nums, right-1);
-                } else if (sum > 0) 
-                    right = moveRight(nums, right-1);
+                    ArrayList<Integer> tmp = new ArrayList();
+                    tmp.add(nums[i]);
+                    tmp.add(nums[l]);
+                    tmp.add(nums[r]);
+                    zero.add(tmp);
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l-1])
+                        l++;
+                    while (l < r && nums[r] == nums[r+1])
+                        r--;
+                } else if (sum < 0)
+                    l++;
                 else
-                    left = moveLeft(nums, left+1);
+                    r--;
             }
-            i = moveLeft(nums, i+1);
         }
-        return res;
-    }
-    
-    private int moveLeft(int[] nums, int left) {
-        while (left < nums.length && nums[left] == nums[left-1])
-            left++;
-        return left;
-    }
-    
-    private int moveRight(int[] nums, int right) {
-        while (right >= 0 && nums[right] == nums[right+1])
-            right--;
-        return right;
+        return zero;
     }
 }
