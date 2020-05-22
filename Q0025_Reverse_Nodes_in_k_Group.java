b794f17ab310dfb8e7f6744a9e75601d1d4e4b30
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-22 16:31:39 -0500

    Update Q0025_Reverse_Nodes_in_k_Group.java

--- Q0025_Reverse_Nodes_in_k_Group.java
+++ Q0025_Reverse_Nodes_in_k_Group.java
@@ -43,3 +43,45 @@ public class Q025_Reverse_Nodes_in_k_Group {
     return tail;
   }
 }
+
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null) return null;
+        ListNode dum = new ListNode(0);
+        dum.next = head;
+        int cnt = 0;
+        ListNode pre = dum;
+        ListNode cur = pre;
+        while (cur != null) {
+            while (cnt < k) {
+                cur = cur.next;
+                if (cur == null && cnt < k)  // the last few fail to form a group
+                    return dum.next;
+                cnt++;
+            }
+            ListNode nxtStart = cur.next;
+            cur.next = null;
+            ListNode h = pre.next;
+            pre.next = null;
+            ListNode[] part = reverse(h);
+            pre.next = part[0];
+            part[1].next = nxtStart;
+            pre = part[1];
+            cur = part[1];
+            cnt = 0;
+        }
+        return dum.next;
+    }
+    
+    private ListNode[] reverse(ListNode head) {
+        ListNode h2 = null;
+        ListNode t2 = head;
+        while (head != null) {
+            ListNode nxt = head.next;
+            head.next = h2;
+            h2 = head;
+            head = nxt;
+        }
+        return new ListNode[] {h2, t2};
+    }
+}
