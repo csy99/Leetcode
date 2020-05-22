f8b6674d3e6431e77fe2a4ccc4863b32562c87c1
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-22 17:53:57 -0500

    Update Q0086_Partition_List.java

--- Q0086_Partition_List.java
+++ Q0086_Partition_List.java
@@ -8,36 +8,26 @@ import Leetcode.Util.ListNode;
  * https://leetcode.com/problems/partition-list/
  */
 public class Q086_Partition_List {
-  public ListNode partition(ListNode head, int x) {
-    ListNode h1 = new ListNode(0);
-    ListNode t1 = h1;
-    ListNode h2 = new ListNode(0);
-    ListNode t2 = h2;
-    
-    while (head != null) {
-      ListNode next = head.next;
-      if (head.val < x) {
-        t1.next = head;
-        t1 = t1.next;
-        t1.next = null;
-      } else {
-        t2.next = head;
-        t2 = t2.next;
-        t2.next = null;
-      } 
-      head = next;
+    public ListNode partition(ListNode head, int x) {
+        ListNode dum1 = new ListNode(0);
+        ListNode dum2 = new ListNode(0);
+        ListNode t1 = dum1;
+        ListNode t2 = dum2;
+        while (head != null) {
+            ListNode nxt = head.next;
+            head.next = null;
+            if (head.val < x) {
+                t1.next = head;
+                t1 = head;
+            } else {
+                t2.next = head;
+                t2 = head;
+            }
+            head = nxt;
+        }
+        if (dum1.next == null)  // no value smaller than x
+            return dum2.next;
+        t1.next = dum2.next;
+        return dum1.next;
     }
-    // concat all lists
-    ListNode newHead = new ListNode(0);
-    ListNode newTail = newHead;
-    if (h1.next != null) {
-      newHead.next = h1.next;
-      newTail = t1;
-    } 
-    if (h2.next != null) {
-      newTail.next = h2.next;
-      newTail = t2;
-    }
-    return newHead.next;
-  }
 }
