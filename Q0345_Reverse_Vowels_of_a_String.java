c228323d29e43e8d5b80a6310abca72854980f6f
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-24 17:43:19 -0500

    Update Q0345_Reverse_Vowels_of_a_String.java

--- Q0345_Reverse_Vowels_of_a_String.java
+++ Q0345_Reverse_Vowels_of_a_String.java
@@ -7,34 +7,34 @@ import java.util.Set;
  * Created by csy99 on 3/18/20.
  */
 public class Q345_Reverse_Vowels_of_a_String {
-  public String reverseVowels(String s) {
-    char[] str = s.toCharArray();
-    int l = 0;
-    int r = str.length-1;
-    while (l < r) {
-      while (Character.toLowerCase(str[l]) != 'a' && 
-             Character.toLowerCase(str[l]) != 'e' &&
-             Character.toLowerCase(str[l]) != 'i' &&
-             Character.toLowerCase(str[l]) != 'o' &&
-             Character.toLowerCase(str[l]) != 'u') {
-        l++;
-        if (l > str.length-1)
-          return String.valueOf(str);
-      }
-      while (Character.toLowerCase(str[r]) != 'a' && 
-             Character.toLowerCase(str[r]) != 'e' &&
-             Character.toLowerCase(str[r]) != 'i' &&
-             Character.toLowerCase(str[r]) != 'o' &&
-             Character.toLowerCase(str[r]) != 'u') {
-        r--;
-        if (r < 0)
-          return String.valueOf(str);
-      }
-      if (l >= r) break;
-      char tmp = str[l];
-      str[l++] = str[r];
-      str[r--] = tmp;
+    public String reverseVowels(String s) {
+        if (s.isEmpty()) return s;
+        char[] arr = s.toCharArray();
+        int l = 0;
+        int r = arr.length-1;
+        while (l < r) {
+            while (l < r && !isVow(arr[l]))
+                l++;
+            while (l < r && !isVow(arr[r]))
+                r--;
+            if (l < r) {
+                char tmp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+        return new String(arr);
     }
-    return String.valueOf(str);
-  }
+    
+    private boolean isVow(char c) {
+        if (c == 'a' || c == 'A' ||
+           c == 'e' || c == 'E' ||
+           c == 'i' || c == 'I' ||
+           c == 'o' || c == 'O' ||
+           c == 'u' || c == 'U')
+            return true;
+        return false;
+    }
 }
