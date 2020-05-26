cdae9bc628800d093b4bf794d531e91762b66e8f
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-26 17:53:53 -0500

    Update Q0271_Encode_and_Decode_Strings.java

--- Q0271_Encode_and_Decode_Strings.java
+++ Q0271_Encode_and_Decode_Strings.java
@@ -7,38 +7,29 @@ import java.util.List;
  * Created by csy99 on 3/19/20.
  */
 public class Q271_Encode_and_Decode_Strings {
-    /*
-     * @param strs: a list of strings
-     * @return: encodes a list of strings to a single string.
-     */
     public String encode(List<String> strs) {
-      if (strs == null || strs.size() == 0) return "";
-      StringBuilder sb = new StringBuilder();
-      for (int i = 0; i < strs.size(); i++) {
-        if (strs.get(i).equals(":") || strs.get(i).equals(";"))
-          sb.append(strs.get(i));
-        sb.append(strs.get(i));
-        sb.append(":;");
-      }
-      return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strs.size(); i++) {
+            sb.append(strs.get(i).length());
+            sb.append(";").append(strs.get(i));
+        }
+        return sb.toString();
     }
 
-    /*
-     * @param str: A string
-     * @return: dcodes a single string to a list of strings
-     */
     public List<String> decode(String str) {
-      List<String> res = new ArrayList<String>();
-      if (str.equals("")) return res;
-      String[] arr = str.split(":;");
-      for (int i = 0; i < arr.length; i++) {
-        if (arr[i].equals("::"))
-          res.add(":");
-        else if (arr[i].equals(";;"))
-          res.add(";");  
-        else
-          res.add(arr[i]);
-      }
-      return res;
+        List<String> res = new ArrayList();
+        int i = 0;
+        while (i < str.length()) {
+            int len = str.charAt(i) - '0';
+            while (i+1 < str.length() && str.charAt(i+1) != ';') {
+                i++;
+                len = len*10 + str.charAt(i) - '0';
+            }
+            i++;  // i points to delimeter
+            String sub = str.substring(i+1, i+len+1);
+            res.add(sub);
+            i += len + 1;
+        }
+        return res;
     }
 }
