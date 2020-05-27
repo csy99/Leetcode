f273f67d88dd313c0fb918ef1e1f0935c41e7c71
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-27 15:14:07 -0500

    Update Q0158_Read_N_Characters_Given_Read4_II_Call_multiple_times.java

--- Q0158_Read_N_Characters_Given_Read4_II_Call_multiple_times.java
+++ Q0158_Read_N_Characters_Given_Read4_II_Call_multiple_times.java
@@ -13,32 +13,23 @@ package Leetcode;
  * The read function may be called multiple times.
  */
 public class Q158_Read_N_Characters_Given_Read4_II_Call_multiple_times {
-    /**
-     * @param buf Destination buffer
-     * @param n   Maximum number of characters to read
-     * @return The number of characters read
-     */
-    private int buffPtr = 0;
-    private int buffCnt = 0;
-    private char[] buff = new char[4];
-
+    int pos = 0;
+    int cnt = 0;
+    char[] cur = new char[4];
     public int read(char[] buf, int n) {
         int total = 0;  // # of chars we have read
         while (total < n) {
-            if (buffPtr == 0) 
-                buffCnt = read4(buff);
-            
-            if (buffCnt == 0) break;  // cannot read from file any more
-            while (total < n && buffPtr < buffCnt) {
-                buf[total++] = buff[buffPtr++];
-            }
-            if (buffPtr >= buffCnt) buffPtr = 0;  // consume all chars in this read
-            if (buffCnt < 4) break;  // exit the loop earlier by catching last read
+            if (pos == 0)
+                cnt = read4(cur);
+            if (cnt == 0)  // cannot read from file any more
+                break;
+            while (total < n && pos < cnt)
+                buf[total++] = cur[pos++];
+            if (pos >= cnt)  // consume all chars in this read
+                pos = 0;
+            if (cnt < 4)  // exit the loop earlier by catching last read
+                break;
         }
         return total;
-    }
-
-    private int read4(char[] buff) {
-        return 1;
     }
 }
