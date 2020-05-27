733c3aaddc7f4d244785d064e94b40bf82c3cd1c
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-27 15:37:59 -0500

    Update Q0115_Distinct_Subsequences.java

--- Q0115_Distinct_Subsequences.java
+++ Q0115_Distinct_Subsequences.java
@@ -4,22 +4,23 @@ package Leetcode;
  * Created by csy99 on 3/21/20.
  */
 public class Q115_Distinct_Subsequences {
-  // dp[i][j]: number of subseq of s[1:j] equals t[1:i]
-  public int numDistinct(String s, String t) {
-    int m = t.length();
-    int n = s.length();
-    int[][] ways = new int[m+1][n+1];
-    for (int j = 0; j < n+1; j++)
-      ways[0][j] = 1;
-    
-    for (int i = 1; i < m+1; i++) {
-      for (int j = 1; j < n+1; j++) {
-        if (t.charAt(i-1) == s.charAt(j-1))
-          ways[i][j] = ways[i-1][j-1] + ways[i][j-1];  // latter term: do not use s[j] to match t[i]
-        else
-          ways[i][j] = ways[i][j-1];
-      }
+    public int numDistinct(String s, String t) {
+        int res = 0;
+        int m = s.length();
+        int n = t.length();
+        if (m == 0 || n == 0 || m < n)
+            return 0;
+        // dp[i][j]: number of subseq of s[1:i] equals t[1:j]
+        int[][] cnt = new int[m+1][n+1];
+        for (int i = 0; i <= m; i++)  // using chars in s to match "" in t
+            cnt[i][0] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= Math.min(i,n); j++) {
+                cnt[i][j] = cnt[i-1][j];
+                if (s.charAt(i-1) == t.charAt(j-1))
+                    cnt[i][j] += cnt[i-1][j-1];
+            }
+        }
+        return cnt[m][n];
     }
-    return ways[m][n];
-  }
 }
