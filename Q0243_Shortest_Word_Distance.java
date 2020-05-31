7bead078ddaffe1076e062a90a4054a5d2300d58
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-05-31 14:49:06 -0500

    Update and rename Q243_Shortest_Word_Distance.java to Q0243_Shortest_Word_Distance.java

--- Q0243_Shortest_Word_Distance.java
+++ Q0243_Shortest_Word_Distance.java
@@ -17,17 +17,17 @@ public class Q243_Shortest_Word_Distance {
     public int shortestDistance(String[] words, String word1, String word2) {
         int index1 = -1;
         int index2 = -1;
-        int minLen = Integer.MAX_VALUE;
+        int minLen = words.length;
         for (int i = 0; i < words.length; i++) {
           if (words[i].equals(word1)) {
             if (index2 != -1)
               minLen = Math.min(minLen, i - index2);
-            index1 = i;
+              index1 = i;
           }
           else if (words[i].equals(word2)) {
             if (index1 != -1)
               minLen = Math.min(minLen, i - index1);
-            index2 = i;
+              index2 = i;
           }
         }
         
