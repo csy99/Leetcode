6921ad258c8300742d42896f6cf1422154896c17
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-05 14:51:02 -0500

    Update Q0309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java

--- Q0309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
+++ Q0309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.java
@@ -19,16 +19,16 @@ public class Q309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown {
   
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        if (n <= 1) return 0;
-        // dp[i]: 0, rest; 1, hold stocks; 2, sell
-        int[][] dp = new int[n+1][3];
-        for (int i = 0; i <= n; i++)
-            dp[i][1] = Integer.MIN_VALUE;
-        for (int i = 1; i <= n; i++) {
-            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][2]);
-            dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i-1]);
-            dp[i][2] = dp[i-1][1] + prices[i-1];
+        if (n <= 1) return 0; 
+        int[] rest = new int[n];
+        int[] hold = new int[n];
+        int[] sold = new int[n];
+        hold[0] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            rest[i] = Math.max(rest[i-1], sold[i-1]);
+            hold[i] = Math.max(hold[i-1], rest[i-1] - prices[i]);
+            sold[i] = prices[i]+hold[i-1];
         }
-        return Math.max(dp[n][0], dp[n][2]);
+        return Math.max(rest[n-1], sold[n-1]);
     }
 }
