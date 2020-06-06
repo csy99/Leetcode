736ad8937574ec0ed3b31bdd5d9b1988c2174a59
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-06 14:25:15 -0500

    Update and rename Q188_Best_Time_to_Buy_and_Sell_Stock_IV.java to Q0188_Best_Time_to_Buy_and_Sell_Stock_IV.java

--- Q0188_Best_Time_to_Buy_and_Sell_Stock_IV.java
+++ Q0188_Best_Time_to_Buy_and_Sell_Stock_IV.java
@@ -21,3 +21,24 @@ public class Q188_Best_Time_to_Buy_and_Sell_Stock_IV {
     return sell[MAX_TRANS];
   }
 }
+
+    public int maxProfit(int k, int[] prices) {
+        int n = prices.length;
+        if (n < 2) return 0;
+        // dp[i][j]: in i-th day using j transaction
+        int K = Math.min(k, n/2);  // pruning, otherwise TLE
+        int[][] hold = new int[n][K+1];
+        int[][] notHold = new int[n][K+1];
+        for (int j = 0; j <= K; j++)
+            hold[0][j] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= K; j++) {
+                notHold[i][j] = Math.max(notHold[i-1][j], hold[i-1][j]+prices[i]);
+                hold[i][j] = Math.max(hold[i-1][j], notHold[i-1][j-1]-prices[i]);
+            }
+        }
+        int res = 0;
+        for (int j = 0; j <= K; j++)
+            res = Math.max(res, notHold[n-1][j]);
+        return res;
+    }
