5c40f80ca14b26db938ca74079c9f041b0ffcf24
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-06 14:29:49 -0500

    Update Q0011_Container_With_Most_Water.java

--- Q0011_Container_With_Most_Water.java
+++ Q0011_Container_With_Most_Water.java
@@ -4,18 +4,19 @@ package Leetcode;
  * Created by csy99 on 2/24/20.
  */
 public class Q011_Container_With_Most_Water {
-public int maxArea(int[] height) {
-      int max = 0;
-      int left = 0; 
-      int right = height.length-1;
-      while (left < right) {
-        int area = Math.min(height[left], height[right]) * (right - left);
-        max = Math.max(max, area);
-        if (height[left] < height[right])
-          left++;
-        else
-          right--;
-      }
-      return max;
+    public int maxArea(int[] height) {
+        int n = height.length;
+        int res = 0;
+        int l = 0;
+        int r = n-1;
+        while (l < r) {
+            int area = (r-l)*Math.min(height[l], height[r]);
+            res = Math.max(res, area);
+            if (height[l] <= height[r])
+                l++;
+            else
+                r--;
+        }
+        return res;
     }
 }
