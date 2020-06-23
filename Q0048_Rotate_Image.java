24c7c96483d8be2e47e890fac70a3612cd012a21
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-22 21:16:55 -0500

    Update Q0048_Rotate_Image.java

--- Q0048_Rotate_Image.java
+++ Q0048_Rotate_Image.java
@@ -6,23 +6,26 @@ package Leetcode;
  * You are given an n x n 2D matrix representing an image.
  */
 public class Q0048_Rotate_Image {
-  public void rotate(int[][] matrix) {
-    int n = matrix.length;
-    if (n == 0) return;
-    int topLeft = 0;
-    int botRight = n - 1;
-    int level = n;
-    while (level > 1) {
-      for (int i = 0; i < level-1; i++) {
-        int tmp = matrix[topLeft][topLeft+i];
-        matrix[topLeft][topLeft+i] = matrix[botRight-i][topLeft];
-        matrix[botRight-i][topLeft] = matrix[botRight][botRight-i];
-        matrix[botRight][botRight-i] = matrix[topLeft+i][botRight];
-        matrix[topLeft+i][botRight] = tmp;
-      }
-      topLeft++;
-      botRight--;
-      level -= 2;
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        if (n < 2) return;
+        int top = 0;
+        int bot = n-1;
+        int left = 0;
+        int right = n-1;
+        int level = n;
+        while (left < right) {
+            for (int i = 0; i < level-1; i++) {
+                int tmp = matrix[top][left+i];
+                matrix[top][left+i] = matrix[bot-i][left];
+                matrix[bot-i][left] = matrix[bot][right-i];
+                matrix[bot][right-i] = matrix[top+i][right];
+                matrix[top+i][right] = tmp;
+            }
+            left++;
+            right--;
+            top++;
+            bot--;
+            level -= 2;
+        }
     }
-  }
-}
