e978041108bb3f4196571e7e51c1c8bf74b940d9
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-24 16:55:23 -0500

    Update Q0037_Sudoku_Solver.java

--- Q0037_Sudoku_Solver.java
+++ Q0037_Sudoku_Solver.java
@@ -4,47 +4,49 @@ package Leetcode;
  * Created by csy99 on 4/1/20.
  */
 public class Q037_Sudoku_Solver {
-  boolean[][] rows = new boolean[9][10];
-  boolean[][] cols = new boolean[9][10];
-  boolean[][] box = new boolean[9][10];
-  public void solveSudoku(char[][] board) {
-    for (int i = 0; i < 9; i ++) {
-      for (int j = 0; j < 9; j++) {
-        char c = board[i][j];
-        if (c != '.') {
-          int num = c - '0';
-          rows[i][num] = true;
-          cols[j][num] = true;
-          box[i/3*3 + j/3][num] = true;
+    boolean[][] row = new boolean[9][10];
+    boolean[][] col = new boolean[9][10];
+    boolean[][] box = new boolean[9][10];
+    
+    public void solveSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) 
+            for (int j = 0; j < 9; j++) 
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0';
+                    row[i][num] = true;
+                    col[j][num] = true;
+                    int boxKey = boxKey(i, j);
+                    box[boxKey][num] = true;
+                }
+        dfs(board, 0, 0);
+    }
+    
+    private boolean dfs(char[][] board, int y, int x) {
+        if (y == 9) return true;
+        int nx = (x+1)%9;
+        int ny = nx == 0 ? y+1:y;
+        if (board[y][x] != '.') 
+            return dfs(board, ny, nx);
+        
+        for (int num = 1; num < 10; num++) {
+            int boxKey = boxKey(y, x);
+            if (row[y][num] || col[x][num] || box[boxKey][num])
+                continue;
+            row[y][num] = true;
+            col[x][num] = true;
+            box[boxKey][num] = true;
+            board[y][x] = (char)('0' + num);
+            if (dfs(board, ny, nx))
+                return true;
+            board[y][x] = '.';
+            box[boxKey][num] = false;
+            col[x][num] = false;
+            row[y][num] = false;
         }
-      }
+        return false;
     }
-    fill(board, 0, 0);
-  }
-  
-  private boolean fill(char[][] board, int y, int x) {
-    if (y == 9) return true;
-    int nx = (x+1)%9;
-    int ny = nx == 0? y+1 : y;
-    if (board[y][x] != '.')
-      return fill(board, ny, nx);
-    for (int i = 1; i <= 9; i++) {
-      int bx = x/3;
-      int by = y/3;
-      int b_idx = by * 3 + bx;
-      if (!rows[y][i] && !cols[x][i] && !box[b_idx][i]) {
-        rows[y][i] = true;
-        cols[x][i] = true;
-        box[b_idx][i] = true;
-        board[y][x] = (char)(i + '0');
-        if (fill(board, ny, nx))
-          return true;
-        board[y][x] = '.';
-        rows[y][i] = false;
-        cols[x][i] = false;
-        box[b_idx][i] = false;
-      }
+    
+    private int boxKey(int i, int j) {
+        return i / 3 * 3 + j / 3;
     }
-    return false;
-  }
 }
