92ae363a955f071c0f5a60db09d50ae4494813ce
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-24 16:34:48 -0500

    Update Q0036_Valid_Sudoku.java

--- Q0036_Valid_Sudoku.java
+++ Q0036_Valid_Sudoku.java
@@ -7,56 +7,47 @@ import java.util.Set;
  * Created by csy99 on 4/1/20.
  */
 public class Q036_Valid_Sudoku {
-  public boolean isValidSudoku(char[][] board) {
-    if (board == null || board.length != 9 || board[0].length != 9)
-      return false;
-    int n = 9;
-    boolean[] taken; // = new boolean[9];
-    // check row
-    for (int i = 0; i < n; i++) {
-      taken = new boolean[9];
-      for (int j = 0; j < n; j++) {
-        if (Character.isDigit(board[i][j])) {
-          int num = board[i][j] - '0';
-          if (taken[num-1]) 
-            return false;
-          taken[num-1] = true;
+    public boolean isValidSudoku(char[][] board) {
+        boolean[] used = new boolean[10];
+        // check row
+        for (int i = 0; i < 9; i++) {
+            used = new boolean[10];
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0';
+                    if (used[num]) return false;
+                    used[num] = true;
+                }
+            }
         }
-      }
-    }
-    
-    // check column
-    for (int j = 0; j < n; j++) {
-      taken = new boolean[9];
-      for (int i = 0; i < n; i++) {
-        if (Character.isDigit(board[i][j])) {
-          int num = board[i][j] - '0';
-          if (taken[num-1]) 
-            return false;
-          taken[num-1] = true;
+        // check col
+        for (int j = 0; j < 9; j++) {
+            used = new boolean[10];
+            for (int i = 0; i < 9; i++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '0';
+                    if (used[num]) return false;
+                    used[num] = true;
+                }
+            }
         }
-      }
-    }
-    
-    // check 3*3 box
-    for (int b = 0; b < n; b++) {
-      taken = new boolean[9];
-      for (int r = 0; r < 3; r++) {
-        for (int c = 0; c < 3; c++) {
-          char tmp = board[r + b/3*3][c + (b%3)*3];
-          if (tmp != '.') {
-            int num = tmp - '0';
-            if (taken[num-1])
-              return false;
-            taken[num-1] = true;
-          }
+        // check box
+        for (int b = 0; b < 9; b++) {
+            int y = b/3*3;
+            int x = (b%3)*3;
+            used = new boolean[10];
+            for (int i = y; i < y+3; i++) {
+                for (int j = x; j < x+3; j++) {
+                    if (board[i][j] != '.') {
+                        int num = board[i][j] - '0';
+                        if (used[num]) return false;
+                        used[num] = true;
+                    }                    
+                }
+            }
         }
-      }
+        return true;
     }
-    
-    
-    return true;
-  }
 
     public static void main(String[] args) {
         Q036_Valid_Sudoku sol = new Q036_Valid_Sudoku();
