d25fc62af764725efefd6fe6d1ef81a0325e5c47
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-24 20:11:05 -0500

    Update Q0317_Shortest_Distance_from_All_Buildings.java

--- Q0317_Shortest_Distance_from_All_Buildings.java
+++ Q0317_Shortest_Distance_from_All_Buildings.java
@@ -21,88 +21,81 @@ In this example, there are three buildings at (0,0), (0,4), (2,2), and an obstac
 The point (1,2) is an ideal empty land to build a house, as the total travel distance of 3+3+1=7 is minimal. So return 7.
  */
 public class Q317_Shortest_Distance_from_All_Buildings {
-  int m = 0;
-  int n = 0;
-  int[][] canReach;
-  int[][] dist;
-  int[] rowDir = new int[] {0, 0, -1, 1};
-  int[] colDir = new int[] {-1, 1, 0, 0};
-  /**
-   * @param grid: the 2D grid
-   * @return: the shortest distance
-   */
-  public int shortestDistance(int[][] grid) {
-    m = grid.length;
-    if (m == 0) return -1;
-    n = grid[0].length;
-    if (n == 0) return -1;
-    canReach = new int[m][n];
-    dist = new int[m][n];
+    int m;
+    int n;
+    int[][] canReach;
+    int[][] dist;
+    int[] dirs = {-1, 0, 1, 0, -1};
+    public int shortestDistance(int[][] grid) {
+        m = grid.length;
+        if (m == 0) return 0;
+        n = grid[0].length;
+        if (n == 0) return 0;
+        canReach = new int[m][n];
+        dist = new int[m][n];
+        
+        int buildings = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    buildings++;
+                    if (!bfs(grid, i, j))
+                        return -1;
+                }
+            }
+        
+        int minDist = Integer.MAX_VALUE;
+        for (int i = 0; i < m; i++) 
+            for (int j = 0; j < n; j++) 
+                if (canReach[i][j] == buildings && dist[i][j] < minDist)
+                    minDist = dist[i][j];
+        return minDist == Integer.MAX_VALUE ? -1 : minDist;
+    }
     
-    int totalBuilding = 0;
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        if (grid[i][j] == 1) {
-          totalBuilding++;
-          if (!bfs(grid, i, j))
-            return -1;
+    private boolean valid(int[][] grid, int y, int x, boolean[][] visited) {
+        if (y < 0 || x < 0 || y >= m || x >= n)
+            return false;
+        if (grid[y][x] == 2)
+            return false;
+        if (visited[y][x])
+            return false;
+        return true;
+    }
+    
+    private boolean bfs(int[][] grid, int r, int c) {
+        Queue<Integer> hori = new LinkedList();
+        Queue<Integer> vert = new LinkedList();
+        boolean[][] visited = new boolean[m][n];
+        hori.offer(r);
+        vert.offer(c);
+        visited[r][c] = true;
+        int step = 0;
+        while (hori.size() > 0) {
+            int size = hori.size();
+            step++;
+            for (int s = 0; s < size; s++) {
+                Integer y = hori.poll();
+                Integer x = vert.poll();
+                for (int d = 0; d < 4; d++) {
+                    int ny = y+dirs[d];
+                    int nx = x+dirs[d+1];
+                    if (!valid(grid, ny, nx, visited))
+                        continue;
+                    if (grid[ny][nx] == 0) {
+                        hori.offer(ny);
+                        vert.offer(nx);
+                        dist[ny][nx] += step;
+                        canReach[ny][nx]++;
+                    }
+                    visited[ny][nx] = true;
+                }
+            }
         }
-      }
-    }
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (grid[i][j] == 1 && !visited[i][j])
+                    return false;
     
-    int minDist = Integer.MAX_VALUE;
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        if (canReach[i][j] == totalBuilding && dist[i][j] < minDist) 
-          minDist = dist[i][j];
-      }
+        return true;
     }
-    return minDist == Integer.MAX_VALUE? -1:minDist;
-  }
-  
-  private boolean isValid(int[][] grid, int y, int x, boolean[][] visited) {
-    if (y < 0 || y >= m || x < 0 || x >= n)
-      return false;
-    if (grid[y][x] == 2)
-      return false;
-    if (visited[y][x])
-      return false;
-    return true;
-  }
-  
-  private boolean bfs(int[][]grid, int y, int x) {
-    Queue<int[]> q = new LinkedList();
-    boolean[][] visited = new boolean[m][n];
-    q.offer(new int[] {y, x});
-    visited[y][x] = true;
-    int d = 0;
-    while (!q.isEmpty()) {
-      d++;
-      int size = q.size();
-      for (int i = 0; i < size; i++) {
-        int[] cur = q.poll();
-        for (int dir = 0; dir < 4; dir++) {
-          int r = cur[0] + rowDir[dir];
-          int c = cur[1] + colDir[dir];
-          if (!isValid(grid, r, c, visited))
-            continue;
-          if (grid[r][c] == 0) {
-            q.offer(new int[] {r, c});
-            dist[r][c] += d;
-            canReach[r][c]++;
-          } 
-          visited[r][c] = true;
-          
-        }
-      }
-    }
-    
-    for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        if (grid[i][j] == 1 && !visited[i][j])
-          return false;
-      }
-    }
-    return true;
-  }
 }
