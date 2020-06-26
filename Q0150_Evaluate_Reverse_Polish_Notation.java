6d496ac8c8da18aa223e8d0eb734fae10cd2a998
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-25 19:58:23 -0500

    Update Q0150_Evaluate_Reverse_Polish_Notation.java

--- Q0150_Evaluate_Reverse_Polish_Notation.java
+++ Q0150_Evaluate_Reverse_Polish_Notation.java
@@ -8,6 +8,36 @@ import java.util.Stack;
  * https://leetcode.com/problems/evaluate-reverse-polish-notation/
  */
 public class Q150_Evaluate_Reverse_Polish_Notation {
+    // cleaner
+    Stack<Integer> nums = new Stack();
+    public int evalRPN(String[] tokens) {
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i].equals("+") || tokens[i].equals("-") ||
+               tokens[i].equals("*") || tokens[i].equals("/") )
+                nums.push(operation(tokens[i]));
+            else
+                nums.push(Integer.parseInt(tokens[i]));
+        }
+        return nums.pop();
+    }
+    
+    private int operation(String op) {
+        int second = nums.pop();
+        int first = nums.pop();
+        switch(op) {
+            case "+":
+                return first + second;
+            case "-":
+                return first - second;
+            case "*":
+                return first * second;
+            case "/":
+                return first / second;
+        }
+        return 0;
+    }
+  
+  // faster
   public int evalRPN(String[] tokens) {
     Stack<Integer> st = new Stack();
     for (int i = 0; i < tokens.length; i++) {
