f595b9fc9588dfedede6d829fce0b7647f738bc2
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-25 20:14:11 -0500

    Update Q0071_Simplify_Path.java

--- Q0071_Simplify_Path.java
+++ Q0071_Simplify_Path.java
@@ -6,26 +6,27 @@ import java.util.Stack;
  * Created by csy99 on 3/24/20.
  */
 public class Q071_Simplify_Path {
-  public String simplifyPath(String path) {
-    String[] arr = path.split("/+");
-    Stack<String> st = new Stack();
-    for (int i = 0; i < arr.length; i++) {
-      if (arr[i].equals(".") || arr[i].isEmpty())
-        continue;
-      else if (arr[i].equals("..")) {
-        if (!st.isEmpty())
-          st.pop();
-      } else
-        st.push(arr[i]);
+    public String simplifyPath(String path) {
+        String[] arr = path.split("/+");
+        StringBuilder sb = new StringBuilder();
+        Stack<String> st = new Stack();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("..")) {
+                if (st.size() > 0)
+                    st.pop();
+            } else if (arr[i].equals(".") || arr[i].isEmpty())
+                continue;
+            else
+                st.push(arr[i]);
+        }
+        int size = st.size();
+        for (int i = 0; i < size; i++) {
+            sb.insert(0, st.pop());
+            sb.insert(0, "/");
+        }
+        if (sb.length() == 0) return "/";
+        return sb.toString();
     }
-    if (st.size() == 0) return "/";
-    StringBuilder sb = new StringBuilder();
-    while (st.size() != 0) {
-      sb.insert(0, st.pop());
-      sb.insert(0, "/");
-    }
-    return sb.toString();
-  }
 
     public static void main(String[] args) {
         Q071_Simplify_Path sol = new Q071_Simplify_Path();
