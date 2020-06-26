9d8da78feda6c1c2ac218c016562de5ec26b7c5a
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-26 16:03:41 -0500

    Update Q0388_Longest_Absolute_File_Path.java

--- Q0388_Longest_Absolute_File_Path.java
+++ Q0388_Longest_Absolute_File_Path.java
@@ -24,4 +24,46 @@ public class Q388_Longest_Absolute_File_Path {
     }
     return maxLen;
   }
+  
+  
+class Solution {
+    public int lengthLongestPath(String input) {
+        if (input.isEmpty()) return 0;
+        Stack<String> st = new Stack();
+        String[] arr = input.split("\n");
+        int longest = 0;
+        if (arr[0].indexOf(".") > 0)
+            longest = arr[0].length();
+        StringBuilder sb = new StringBuilder(arr[0]);
+        st.push(arr[0]);
+        int preLevel = 0;
+        for (int i = 1; i < arr.length; i++) {
+            int curLevel = 0;
+            int idx = 0;
+            while (true) {
+                int newIdx = 1+arr[i].indexOf('\t', idx);
+                if (newIdx <= 0) break;
+                curLevel++;
+                idx = newIdx;
+            }
+            // System.out.println(curLevel);
+            while (st.size() > 0 && curLevel - preLevel != 1) {
+                String preDir = st.pop();
+                sb.setLength(sb.length() - preDir.length());
+                if (st.size() > 0)  // no slash before root dir
+                    sb.setLength(sb.length() - 1);
+                preLevel--;
+            }
+            String curDir = arr[i].substring(idx);
+            if (st.size() > 0)
+                sb.append("/");
+            sb.append(curDir);
+            st.push(curDir);
+            if (curDir.indexOf(".") > 0 && sb.length() > longest) // must be a file
+                longest = sb.length();
+            preLevel = curLevel;
+        }
+        return longest;
+    }
+}
 }
