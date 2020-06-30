f75efb92bb0d371de160d2feb28ba5c6d2be584b
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-06-30 16:10:36 -0500

    Update Q0590_N-ary Tree Postorder Traversal.java

--- Q0590_N-ary Tree Postorder Traversal.java
+++ Q0590_N-ary Tree Postorder Traversal.java
@@ -2,6 +2,23 @@
  * Created by csy99 on 5/11/20.
  */
 class Solution {
+    // recursive
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList();
+        traverse(root, res);
+        return res;
+    }
+    
+    private void traverse(Node root, List<Integer> res) {
+        if (root == null)
+            return;
+        List<Node> children = root.children;
+        for (int i = 0; i < children.size(); i++)
+            traverse(children.get(i), res);
+        res.add(root.val);
+    }
+    
+    // iterative
     public List<Integer> postorder(Node root) {
         LinkedList<Integer> res = new LinkedList();
         if (root == null) return res;
