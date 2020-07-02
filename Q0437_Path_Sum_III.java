3cb9fa8bede197de30bb3ada4cbff2d425ccf348
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-07-01 19:03:49 -0500

    Update Q0437_Path_Sum_III.java

--- Q0437_Path_Sum_III.java
+++ Q0437_Path_Sum_III.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
  * Created by csy99 on 5/11/20.
  */
 public class Q437_Path_Sum_III {
+    // O(n^2)
     public int pathSum(TreeNode root, int sum) {
         if (root == null)
             return 0;
@@ -24,5 +25,24 @@ public class Q437_Path_Sum_III {
         int left = count(root.left, sum);
         int right = count(root.right, sum);
         return cnt + left + right;
+    }
+    
+    
+    // O(n)
+    public int pathSum(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap();
+        preSum.put(0,1);
+        return helper(root, 0, sum, preSum);
+    }
+    
+    private int helper(TreeNode root, int cur, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) return 0;
+        cur += root.val;
+        int res = preSum.getOrDefault(cur-target, 0);
+        preSum.put(cur, preSum.getOrDefault(cur,0)+1);
+        res += helper(root.left, cur, target, preSum) + 
+            helper(root.right, cur, target, preSum);
+        preSum.put(cur, preSum.get(cur) - 1);
+        return res;
     }
 }
