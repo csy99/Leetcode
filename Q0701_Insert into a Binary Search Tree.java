8a39a1278bb2b5bdd264280bda3d54e3746805de
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-07-06 22:27:33 -0500

    Update Q0701_Insert into a Binary Search Tree.java

--- Q0701_Insert into a Binary Search Tree.java
+++ Q0701_Insert into a Binary Search Tree.java
@@ -3,25 +3,11 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if (root == null)
-            return new TreeNode(val);
-        traverseAndInsert(root, val);
+        if (root == null) return new TreeNode(val);
+        if (val < root.val)
+            root.left = insertIntoBST(root.left, val);
+        else
+            root.right = insertIntoBST(root.right, val);
         return root;
-    }
-    
-    private void traverseAndInsert(TreeNode root, int val) {
-        if (root == null)  // should not happen
-            return;
-        if (val < root.val) {
-            if (root.left == null)
-                root.left = new TreeNode(val);
-            else
-                traverseAndInsert(root.left, val);
-        } else {
-            if (root.right == null)
-                root.right = new TreeNode(val);
-            else
-                traverseAndInsert(root.right, val);
-        }
     }
 }
