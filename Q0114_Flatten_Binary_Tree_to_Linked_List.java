5116cc696c12693dfaf04c7e6c6aba3c7c09f6eb
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-07-09 21:29:36 -0500

    Update Q0114_Flatten_Binary_Tree_to_Linked_List.java

--- Q0114_Flatten_Binary_Tree_to_Linked_List.java
+++ Q0114_Flatten_Binary_Tree_to_Linked_List.java
@@ -9,18 +9,17 @@ import java.util.Stack;
  */
 public class Q114_Flatten_Binary_Tree_to_Linked_List {
     public void flatten(TreeNode root) {
-        if (root == null) return;
-        Stack<TreeNode> st = new Stack();
-        st.push(root);
-        while (!st.isEmpty()) {
-            TreeNode cur = st.pop();
-            if (cur.right != null) 
-                st.push(cur.right);
-            if (cur.left != null) 
-                st.push(cur.left);
-            if (!st.isEmpty())
-                cur.right = st.peek();
-            cur.left = null;
-        }
+        if (root == null) 
+            return;
+        flatten(root.left);
+        flatten(root.right);
+        if (root.left == null) return;
+        TreeNode rightTree = root.right;
+        root.right = root.left;
+        root.left = null;
+        TreeNode lastInLeft = root;
+        while (lastInLeft.right != null)
+            lastInLeft = lastInLeft.right;
+        lastInLeft.right = rightTree;
     }
 }
