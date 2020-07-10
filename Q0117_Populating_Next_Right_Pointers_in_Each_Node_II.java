202c19db17712d5b9b92320e3710b4d06dfc9c47
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-07-10 11:55:15 -0500

    Update Q0117_Populating_Next_Right_Pointers_in_Each_Node_II.java

--- Q0117_Populating_Next_Right_Pointers_in_Each_Node_II.java
+++ Q0117_Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -6,45 +6,40 @@ package Leetcode;
 public class Q117_Populating_Next_Right_Pointers_in_Each_Node_II {
     public Node connect(Node root) {
         if (root == null) return null;
-        Node nextLevelFirst = null;
-        Node pre = null;
-        Node cur = root;
-        
-        while (cur != null) {
+        Node first = root;
+        Node nxtFirst = null;
+        while (first != null) {
+            Node cur = first;
             while (cur != null) {
                 if (cur.left != null) {
-                    if (nextLevelFirst == null) {
-                        nextLevelFirst = cur.left;
-                        pre = cur.left;
-                    } else {
-                        pre.next = cur.left;
-                        pre = pre.next;
-                    }
+                    if (nxtFirst == null)
+                        nxtFirst = cur.left;
+                    cur.left.next = findNext(cur, true);
                 }
                 if (cur.right != null) {
-                    if (nextLevelFirst == null) {
-                        nextLevelFirst = cur.right;
-                        pre = cur.right;
-                    } else {
-                        pre.next = cur.right;
-                        pre = pre.next;
-                    }
-                }
+                    if (nxtFirst == null)
+                        nxtFirst = cur.right;
+                    cur.right.next = findNext(cur, false);
+                }                    
                 cur = cur.next;
             }
-            cur = nextLevelFirst;
-            pre = null;
-            nextLevelFirst = null;
+            first = nxtFirst;
+            nxtFirst = null;
         }
         return root;
     }
-}
-
-class TreeLinkNode {
-    int val;
-    TreeLinkNode left, right, next;
-
-    TreeLinkNode(int x) {
-        val = x;
+    
+    private Node findNext(Node root, boolean leftChild) {
+        if (root == null) return null;
+        if (leftChild && root.right != null)
+            return root.right;
+        Node cur = root.next;
+        while (cur != null) {
+            if (cur.left != null) return cur.left;
+            if (cur.right != null) return cur.right;
+            cur = cur.next;
+        }
+        return null;
     }
 }
+
