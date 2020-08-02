13ce4c1c1d906f15a2d1315447602aa35709bbc2
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-08-02 09:50:41 -0500

    Update Q0010_Regular_Expression_Matching.java

--- Q0010_Regular_Expression_Matching.java
+++ Q0010_Regular_Expression_Matching.java
@@ -6,35 +6,32 @@ package Leetcode;
  * https://leetcode.com/problems/regular-expression-matching/
  */
 public class Q010_Regular_Expression_Matching {
-  public boolean isMatch(String s, String p) {
-    if (s == null || p == null) return false;
-    int m = s.length();
-    int n = p.length();
-    boolean[][] match = new boolean[m+1][n+1];
-    match[0][0] = true;  // "" matches ""
-    for (int j = 1; j <= n; j++) {
-      // assume p won't start with *
-      if (p.charAt(j-1) == '*')
-        match[0][j] = match[0][j-2];
+    public boolean isMatch(String s, String p) {
+        int m = p.length();
+        int n = s.length();
+        boolean[][] match = new boolean[m+1][n+1];
+        match[0][0] = true;  // "" matches ""
+        for (int i = 1; i <= m; i++)
+            if (p.charAt(i-1) == '*')
+                match[i][0] = match[i-2][0];
+        for (int i = 1; i <= m; i++) {
+            char pc = p.charAt(i-1);
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(j-1);
+                if (pc == '.' || pc == sc)
+                    match[i][j] = match[i-1][j-1];
+                else if (pc == '*') {  // assume at least one char before "*"
+                    // "*" combo match to nothing
+                    match[i][j] = match[i-2][j];
+                    char p_prev = p.charAt(i-2);
+                    // "*" combo match to one or more char in s
+                    if (p_prev == '.' || p_prev == sc) 
+                        match[i][j] |= match[i][j-1];
+                }
+            }
+        }
+        return match[m][n];
     }
-    
-    for (int i = 1; i <= m; i++) {
-      for (int j = 1; j <= n; j++) {
-        char sc = s.charAt(i-1);
-        char pc = p.charAt(j-1);
-        if (pc == '.' || sc == pc)
-          match[i][j] = match[i-1][j-1];
-        else if (pc == '*') {
-          char p_prev = p.charAt(j-2);
-          if (p_prev == '.' || p_prev == sc)
-            match[i][j] = match[i-1][j] || match[i][j-2];
-          else
-            match[i][j] = match[i][j-2];
-        } 
-      }
-    }
-    return match[m][n];
-  }
 
     public static void main(String[] args) {
         Q010_Regular_Expression_Matching sol = new Q010_Regular_Expression_Matching();
