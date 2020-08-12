b7cdbcd63667411dd16299261aa026241b5b1cdb
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-08-11 21:55:30 -0500

    Update Q1024_Video_Stitching.java

--- Q1024_Video_Stitching.java
+++ Q1024_Video_Stitching.java
@@ -2,6 +2,30 @@
  * Created by csy99 on 5/8/20. 
  */
 class Solution {
+    // greedy
+    public int videoStitching(int[][] clips, int T) {
+        int n = clips.length;
+        if (n == 0) return -1;
+        Arrays.sort(clips, (c1, c2) -> c1[0] - c2[0]);
+        if (clips[0][0] > 0 || clips[n-1][1] < T)
+            return -1;
+        int end = -1;
+        int potential = 0;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            if (potential >= T || clips[i][0] > potential) 
+                break;
+            if (clips[i][0] > end) {
+                res++;
+                end = potential;
+            }
+            potential = Math.max(potential, clips[i][1]);
+        }
+        if (potential < T) return -1;
+        return res;
+    }
+    
+    // dp
     public int videoStitching(int[][] clips, int T) {
         final int INF = 1000;
         if (T > 0 && clips.length == 0) return -1;
