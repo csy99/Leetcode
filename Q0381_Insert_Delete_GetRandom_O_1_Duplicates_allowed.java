e8834f337c936c9028635b8025a9fc1872403fa1
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-08-18 20:51:06 -0500

    Update Q0381_Insert_Delete_GetRandom_O_1_Duplicates_allowed.java

--- Q0381_Insert_Delete_GetRandom_O_1_Duplicates_allowed.java
+++ Q0381_Insert_Delete_GetRandom_O_1_Duplicates_allowed.java
@@ -43,3 +43,57 @@ public class Q381_Insert_Delete_GetRandom_O_1_Duplicates_allowed {
         return lst.get(rand.nextInt(lst.size()));        
     }
 }
+
+
+class RandomizedCollection {
+
+    HashMap<Integer, List<Integer>> map = new HashMap();
+    ArrayList<Entry> values = new ArrayList();
+    Random rand = new Random();
+    
+    /** Initialize your data structure here. */
+    public RandomizedCollection() {
+        
+    }
+    
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    public boolean insert(int val) {
+        List<Integer> indices = map.getOrDefault(val, new ArrayList<Integer>());
+        indices.add(values.size());
+        map.put(val, indices);
+        values.add(new Entry(val, indices.size()-1));
+        return indices.size() == 1;
+    }
+    
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    public boolean remove(int val) {
+        if (!map.containsKey(val) || map.get(val).size() == 0)
+            return false;
+        List<Integer> indices = map.get(val);
+        int idx_to_evict = indices.get(indices.size()-1);
+        Entry last_entry = values.get(values.size()-1);
+        // update index
+        map.get(last_entry.value).set(last_entry.idx, idx_to_evict);
+        // swap values in arrayList
+        values.set(idx_to_evict, last_entry);
+        // remove
+        values.remove(values.size()-1);
+        indices.remove(indices.size()-1);
+        return true;
+    }
+    
+    /** Get a random element from the collection. */
+    public int getRandom() {
+        int idx = rand.nextInt(values.size());
+        return values.get(idx).value;
+    }
+}
+
+class Entry {
+    public int value;
+    public int idx;
+    public Entry(int v, int i) {
+        value = v;
+        idx = i;
+    }
+}
