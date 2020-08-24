7506ca0ace2ba13d392dce1ffb25cb7ff4aea639
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-08-24 16:04:22 -0500

    Update Q0190_Reverse_Bits.java

--- Q0190_Reverse_Bits.java
+++ Q0190_Reverse_Bits.java
@@ -5,13 +5,13 @@ package Leetcode;
  */
 public class Q190_Reverse_Bits {
     // you need treat n as an unsigned value
+    // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int res = 0;
         for (int i = 0; i < 32; i++) {
-            res += n & 1;
-            n >>>= 1;
-            if (i < 31)
-                res <<= 1;
+            // in round i, (n & 1) extracts the i-th digit from the end
+            res = (res << 1) | (n & 1);
+            n >>= 1;
         }
         return res;
     }
