5e6c049ad9d3013bd2da25155973212a598c9aab
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-09-11 11:51:17 -0500

    Update Q0023_Merge_k_Sorted_Lists.java

--- Q0023_Merge_k_Sorted_Lists.java
+++ Q0023_Merge_k_Sorted_Lists.java
@@ -9,45 +9,43 @@ import Leetcode.Util.ListNode;
  * and return it as one sorted list. Analyze and describe its complexity.
  */
 public class Q023_Merge_k_Sorted_Lists {
-  public ListNode mergeKLists(ListNode[] lists) {
-    if (lists.length == 0)
-      return null;
-    int interval = 1;
-    while (interval < lists.length) {
-      for (int i = 0; i < lists.length - interval; i += interval*2) {
-        lists[i] = mergeTwoLists(lists[i], lists[i+interval]);
-      }
-      interval *= 2;
+
+// extension for merging 2 lists
+// time: O(n*log k), space: O(1)
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int n = lists.length;
+        if (n == 0) return null;
+        int interval = 1;
+        while (interval < n) {
+            for (int i = 0; i < n-interval; i+=interval*2) 
+                lists[i] = merge2Lists(lists[i], lists[i+interval]);
+            interval *= 2;
+        }
+        return lists[0];
     }
-    return lists[0];    
-  }
     
-  // leetcode 21  
-  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    if (l1 == null)
-      return l2;
-    if (l2 == null)
-      return l1;
-    ListNode dum = new ListNode(-1);
-    ListNode cur = dum;
-    while (l1 != null && l2 != null) {
-      int a = l1 == null ? Integer.MAX_VALUE : l1.val;
-      int b = l2 == null ? Integer.MAX_VALUE : l2.val;
-      if (a < b) {
-        cur.next = l1;
-        l1 = l1.next;  // won't throw nullPointerException, if l1 is null, a > b
-      } else {
-        cur.next = l2;
-        l2 = l2.next;
-      }
-      cur = cur.next;
+    private ListNode merge2Lists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        ListNode dummy = new ListNode(2);
+        ListNode tail = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;    
+            } else {
+                tail.next = l2;
+                l2 = l2.next;                
+            }
+            tail = tail.next;
+        }
+        if (l1 != null)
+            tail.next = l1;
+        if (l2 != null)
+            tail.next = l2;
+        return dummy.next;
     }
-    if (l1 != null)
-      cur.next = l1;
-    if (l2 != null)
-      cur.next = l2;
-    return dum.next;
-  }  
 }
 
 // priority queue
