0b49adeeddc8b20c37d11d9f00c4c3a45d234257
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-09-21 16:28:54 -0500

    Update Q1004_Max Consecutive Ones III.java

--- Q1004_Max Consecutive Ones III.java
+++ Q1004_Max Consecutive Ones III.java
@@ -3,26 +3,24 @@
  */
 class Solution {
     public int longestOnes(int[] A, int K) {
+        int n = A.length;
         int l = 0;
         int r = 0;
-        int n = A.length;
         int longest = 0;
         int swap = 0;
         
         while (r < n) {
-            if (A[r] == 1) {
+            if (A[r] == 1 || swap < K) {
+                if (A[r] == 0)
+                    swap++;
                 r++;
                 longest = Math.max(r-l, longest);
-            } else if (swap < K) {
-                r++;
-                swap++;
-                longest = Math.max(r-l, longest);
-            } else {
-                while (swap >= K) {
-                    if (A[l] == 0)
-                        swap--;
-                    l++;
-                }
+                continue;
+            }
+            while (swap >= K) {
+                if (A[l] == 0)
+                    swap--;
+                l++;
             }
         }
         return longest;
