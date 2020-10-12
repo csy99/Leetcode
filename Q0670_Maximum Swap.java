3351a7d1f833e97f0d935067bc69650d7fd16c1c
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-10-12 16:04:13 -0500

    Update Q0670_Maximum Swap.java

--- Q0670_Maximum Swap.java
+++ Q0670_Maximum Swap.java
@@ -30,3 +30,32 @@ class Solution {
         sb.setCharAt(j, c1);
     }
 }
+
+class Solution {
+    public int maximumSwap(int num) {
+        String str = String.valueOf(num);
+        int n = str.length();
+        int[] larger = new int[n];
+        for (int i = 0; i < n; i++)
+            larger[i] = -1;
+        for (int i = n-2; i >= 0; i--) {
+            int nxt = Integer.parseInt(str.substring(i+1, i+2));
+            if (larger[i+1] < 0 || Integer.parseInt(str.substring(larger[i+1], larger[i+1]+1)) < nxt) 
+                larger[i] = i+1;
+            else
+                larger[i] = larger[i+1];
+        }
+        StringBuilder sb = new StringBuilder(str);
+        for (int i = 0; i < n; i++) {
+            if (larger[i] < 0) continue;
+            char candidate = str.charAt(larger[i]);
+            char cur = str.charAt(i);
+            if (candidate > cur) {
+                sb.setCharAt(i, candidate);
+                sb.setCharAt(larger[i], cur);
+                break;
+            }
+        }
+        return Integer.parseInt(sb.toString());
+    }
+}
