1f70c9e2432a7427d4d37ef934134820c191f2e5
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-10-20 09:24:24 -0500

    Create Q0549_Binary Tree Longest Consecutive Sequence II.java

---
+++ Q0549_Binary Tree Longest Consecutive Sequence II.java
@@ -0,0 +1,6 @@
+/**
+ * Created by csy99 on 10/20/20.
+ *
+ * Given a binary tree, find the length(number of nodes) of the longest consecutive sequence(Monotonic and adjacent node values differ by 1) path.
+ * The path could be start and end at any node in the tree.
+ */
