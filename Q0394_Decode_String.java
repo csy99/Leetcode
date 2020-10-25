ef67d3918bba67884108386dd71eebc4bf236d7a
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-10-24 23:41:33 -0500

    Update Q0394_Decode_String.java

--- Q0394_Decode_String.java
+++ Q0394_Decode_String.java
@@ -6,31 +6,32 @@ import java.util.Stack;
  * Created by csy99 on 3/24/20.
  */
 public class Q394_Decode_String {
-  public String decodeString(String s) {
-    Stack<String> str = new Stack();
-    Stack<Integer> cnt = new Stack();
-    StringBuilder tail = new StringBuilder();
-    char[] arr = s.toCharArray();
-    for (int i = 0; i < arr.length; i++) {
-      if (Character.isDigit(arr[i])) {
-        int num = arr[i] - '0';
-        while (i+1 < arr.length && Character.isDigit(arr[i+1])) {
-          num = num*10 + (arr[i+1] - '0');
-          i++;
+    public String decodeString(String s) {
+        StringBuilder tail = new StringBuilder();
+        Stack<String> str = new Stack();
+        Stack<Integer> cnt = new Stack();
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < n; i++) {
+            if ('0' <= arr[i] && arr[i] <= '9') {
+                int num = arr[i] - '0';
+                while (i+1 < n && Character.isDigit(arr[i+1])) {
+                    num = num * 10 + (arr[i+1] - '0');
+                    i++;
+                }
+                cnt.push(num);
+            } else if (arr[i] == '[') {
+                str.push(tail.toString());
+                tail.setLength(0);
+            } else if (arr[i] == ']') {
+                StringBuilder part = new StringBuilder(str.pop());
+                int repeated = cnt.pop();
+                for (int r = 0; r < repeated; r++)
+                    part.append(tail);
+                tail = part;
+            } else
+                tail.append(arr[i]);
         }
-        cnt.push(num);
-      } else if (arr[i] == '[') {
-        str.push(tail.toString());
-        tail = new StringBuilder();
-      } else if (arr[i] == ']') {
-        StringBuilder element = new StringBuilder(str.pop());
-        int repeated = cnt.pop();
-        for (int j = 0; j < repeated; j++) 
-          element.append(tail);
-        tail = element;
-      } else
-        tail.append(arr[i]);
+        return tail.toString();
     }
-    return tail.toString();
-  }
 }
