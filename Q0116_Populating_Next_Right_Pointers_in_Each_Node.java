a5004b1004e68086f5e8f45bdcf3368267383a65
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-10-29 22:56:08 -0500

    Update Q0116_Populating_Next_Right_Pointers_in_Each_Node.java

--- Q0116_Populating_Next_Right_Pointers_in_Each_Node.java
+++ Q0116_Populating_Next_Right_Pointers_in_Each_Node.java
@@ -3,8 +3,34 @@ package Leetcode;
 /**
  * Created by csy99 on 4/8/20.
  */
-public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
 
+// iterative
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) return null;
+        Queue<Node> q = new LinkedList();
+        q.add(root);
+        while (q.size() > 0) {
+            int size = q.size();
+            Node prevRight = null;
+            for (int s = 0; s < size; s++) {
+                Node cur = q.poll();
+                if (cur.left == null) break;
+                if (prevRight != null)
+                    prevRight.next = cur.left;
+                cur.left.next = cur.right;
+                prevRight = cur.right;
+                q.add(cur.left);
+                q.add(cur.right);
+            }
+        }
+        return root;
+    }
+}
+
+// recursive
+public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
+    
     public Node connect(Node root) {
         if (root == null || root.left == null) return root;
         root.left.next = root.right;
@@ -36,10 +62,10 @@ public class Q116_Populating_Next_Right_Pointers_in_Each_Node {
         }
 
     }
+}
 
-    class TreeLinkNode {
-        TreeLinkNode left;
-        TreeLinkNode right;
-        TreeLinkNode next;
-    }
+class TreeLinkNode {
+    TreeLinkNode left;
+    TreeLinkNode right;
+    TreeLinkNode next;
 }
