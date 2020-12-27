6c578b078a5148f357f7e144e9e599a741769e87
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2020-12-26 22:12:04 -0600

    Create Q1705_Maximum Number of Eaten Apples.java

---
+++ Q1705_Maximum Number of Eaten Apples.java
@@ -0,0 +1,40 @@
+/**
+ * Created by csy99 on 12/26/20.
+ */
+class Solution {
+    public int eatenApples(int[] apples, int[] days) {
+        int n = apples.length;
+        TreeMap<Integer, Integer> total = new TreeMap();
+        int i = 0;
+        int res = 0;
+        for (; i < n; i++) {
+            total.remove(i);
+            Integer remaining = total.get(i+days[i]);
+            if (remaining == null) 
+                remaining = 0;
+            if (days[i] != 0)
+                total.put(i+days[i], remaining+apples[i]);
+            Integer todayKey = total.ceilingKey(i);
+            if (todayKey == null) continue;
+            res++;
+            int todayVal = total.get(todayKey);
+            if (todayVal > 1)
+                total.put(todayKey, todayVal-1);
+            else
+                total.remove(todayKey);
+        }
+        while (total.size() > 0) {
+            total.remove(i);
+            Integer todayKey = total.ceilingKey(i);
+            if (todayKey == null) continue;
+            res++;
+            int todayVal = total.get(todayKey);
+            if (todayVal > 1)
+                total.put(todayKey, todayVal-1);
+            else
+                total.remove(todayKey);
+            i++;
+        }
+        return res;
+    }
+}
