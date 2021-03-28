3e604f094a099a6d5a37357470c35892056e79cb
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2021-03-28 16:31:59 -0500

    Update and rename Q461_Hamming_Distance.java to Q0461_Hamming_Distance.java

--- Q0461_Hamming_Distance.java
+++ Q0461_Hamming_Distance.java
@@ -1,21 +1,21 @@
 package Leetcode;
 
 /**
- * Created by rbhatnagar2 on 1/15/17.
+ * Created by csy99 on 3/28/21.
  * <p>
  * The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
  * Given two integers x and y, calculate the Hamming distance.
  */
 public class Q461_Hamming_Distance {
     public int hammingDistance(int x, int y) {
-        int result = 0;
-        int xor = x ^ y;
-
-        while (xor > 0) {
-            result += xor & 1;
-            xor = xor >> 1;
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            int a = x & 1;
+            int b = y & 1;
+            if (a != b) res++;
+            x >>>= 1;
+            y >>>= 1;
         }
-        return result;
+        return res;
     }
-
 }
