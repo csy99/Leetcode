16e95c00d85f017caac3db1f95f9aeb31bad9063
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2021-05-18 13:09:18 +0800

    Update Q0224_Basic_Calculator.java

--- Q0224_Basic_Calculator.java
+++ Q0224_Basic_Calculator.java
@@ -79,3 +79,63 @@ class Pair {
 }
 }
 
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> nums = new Stack();
+        Stack<Character> ops = new Stack();
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int i = 0;
+        boolean prev_op = false;
+        
+        while (i < n) {
+            if (arr[i] == ' ') {
+                i++;
+                continue;
+            } else if (arr[i] >= '0' && arr[i] <= '9') {
+                int num = 0;
+                while (i < n && arr[i] >= '0' && arr[i] <= '9') {
+                    num = num*10 + (arr[i]-'0');
+                    i++;
+                }
+                nums.push(num);
+                prev_op = false;
+                i--;
+            } else if (arr[i] == '(') {
+                ops.push(arr[i]);
+                prev_op = true;
+            } else if (arr[i] == ')') {
+                evaluate(nums, ops);
+                prev_op = false;
+            } else {
+                // to address sth like 1+(-9+7)
+                if (prev_op)  
+                    nums.push(0);
+                ops.push(arr[i]);
+                prev_op = true;
+            }
+            i++;
+        }
+        evaluate(nums, ops);
+        return nums.peek();
+    }
+    
+    private void evaluate(Stack<Integer> nums, Stack<Character> ops) {
+        int cur = 0;
+        int num2 = 0;
+        char op = '+';
+        while (ops.size() > 0 && ops.peek() != '(') {
+            op = ops.pop();
+            num2 = nums.pop();
+            if (op == '+')
+                cur += num2;
+            else
+                cur -= num2;
+        }
+        if (nums.size() > 0)
+            cur += nums.pop();
+        if (ops.size() > 0 && ops.peek() == '(')
+            ops.pop();
+        nums.push(cur);
+    }
+}
