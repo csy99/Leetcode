fdaaa5525d3569cdee1d01249bc7c285f0cd5e35
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2021-07-28 14:35:25 +0800

    Update and rename Q525_Contiguous_Array.java to Q0525_Contiguous_Array.java

---
+++ Q0525_Contiguous_Array.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+/**
+ * Created by csy99 on 7/28/21.
+ */
+class Solution {
+    public int findMaxLength(int[] nums) {
+        int n = nums.length;
+        int res = 0;
+        int score = 0;
+        Map<Integer, Integer> pos = new HashMap();
+        pos.put(0, -1);
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 1) score++;
+            else score--;
+            if (pos.containsKey(score))
+                res = Math.max(res, i-pos.get(score));
+            else
+                pos.put(score, i);
+        }
+        return res;
+    }
