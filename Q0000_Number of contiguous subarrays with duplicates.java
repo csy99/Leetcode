45a9fbd7405cec9959031dbd7d880e98ff37a665
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2021-08-11 21:38:48 -0700

    Update Q0000_Number of contiguous subarrays with duplicates.java

--- Q0000_Number of contiguous subarrays with duplicates.java
+++ Q0000_Number of contiguous subarrays with duplicates.java
@@ -1,5 +1,16 @@
 /**
  * Created by csy99 on 8/11/21.
+ 
+ You are given an array of positive/negative numbers that are not necessarily distinct. We have to return the number of contiguous subarrays that contain at least one of each number (i.e. only duplicates).
+
+Examples:
+
+[0, 0, 0] => 3: first two 0s, second and third 0, and entire array
+[1, 2, 3] => 0
+[1, 2, 3, 3, 3, 2, 4, 1] => 4: [3, 3] (first), [3, 3] (second), [3, 3, 3], [2, 3, 3, 3, 2]
+[1, 2, 1, 2] => 1: only the entire array works here
+arr.length <= 10^3
+
  */
 public class Solution {
     public int contiguous(int[] arr) {
