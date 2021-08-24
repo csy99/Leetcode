6e4ea55957c2960c0e9736a25bf65c0faa64051e
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2021-08-24 10:24:50 -0700

    Update Q0426_Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List.java

--- Q0426_Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List.java
+++ Q0426_Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List.java
@@ -33,3 +33,33 @@ public class Q426_Convert_Binary_Search_Tree_to_Sorted_Doubly_Linked_List {
         return dummy.right;
     }
 }
+
+class Solution {
+    public Node treeToDoublyList(Node root) {
+        if (root == null) return null;
+        Node[] nodes = helper(root);
+        nodes[0].left = nodes[1];
+        nodes[1].right = nodes[0];
+        return nodes[0];
+    }
+    
+    private Node[] helper(Node root) {
+        if (root == null) return new Node[]{null, null};
+        Node[] l = helper(root.left);
+        Node[] r = helper(root.right);
+        root.left = l[1];
+        if (l[1] != null)
+            l[1].right = root;
+        root.right = r[0];
+        if (r[0] != null)
+            r[0].left = root;
+        
+        Node head = root;
+        if (l[0] != null)
+            head = l[0];
+        Node tail = root;
+        if (r[1] != null)
+            tail = r[1];
+        return new Node[] {head, tail};
+    }
+}
