830d5b89009b0b78b0aadae173073814cb79615c
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2021-09-02 21:12:10 -0700

    Create Q0_Utils.java

---
+++ Q0_Utils.java
@@ -0,0 +1,22 @@
+class Utils {
+    Integer comb[max][max];
+    
+    static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+  
+    static int lcm(int a, int b, int gcdValue) {
+        return Math.abs(a * b) / gcdValue;
+    } 
+  
+    
+    static int combination(int n, int k) {
+        if (k > n-k) return combination(n, n-k);
+        if(r==0) return comb[n][r] = 1;
+        if(r==1) return comb[n][r] = n;
+        if(comb[n][r] != null) return comb[n][r]; 
+        return comb[n][r] = combination(n-1,r) + combination(n-1,r-1);
+    }
+}
