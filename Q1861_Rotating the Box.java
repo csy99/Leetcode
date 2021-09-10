25a605426becbe9fc56cd352ddb0e4642c67d2a0
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2021-09-09 21:25:47 -0700

    Update Q1861_Rotating the Box.java

--- Q1861_Rotating the Box.java
+++ Q1861_Rotating the Box.java
@@ -2,6 +2,7 @@
  * Created by csy99 on 9/8/21.
  */
 class Solution {
+    // simulation, time: O(m^2n), space: O(mn)
     public char[][] rotateTheBox(char[][] box) {
         int n = box.length;
         int m = box[0].length;
@@ -21,6 +22,29 @@ class Solution {
                 }
             }
         }
+        return graph;
+    }
+    
+    // two pointer, time: O(mn), sapce: O(mn)
+    public char[][] rotateTheBox(char[][] box) {
+        int m = box.length;
+        int n = box[0].length;
+        for (int i = 0; i < m; i++) {
+            int bot = n-1;
+            for (int j = n-1; j >= 0; j--) {
+                if (box[i][j] == '*')
+                    bot = j-1;
+                if (box[i][j] == '#') {
+                    box[i][j] = '.';
+                    box[i][bot] = '#';
+                    bot--;
+                }
+            }
+        }
+        char[][] graph = new char[n][m];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                graph[j][m-i-1] = box[i][j];
         return graph;
     }
 }
