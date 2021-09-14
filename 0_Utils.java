b4f632e2596e7c6e30e78a5c4ed0a685ac799ad8
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2021-09-13 20:35:28 -0700

    Update 0_Utils.java

--- 0_Utils.java
+++ 0_Utils.java
@@ -1,6 +1,14 @@
 class Utils {
     Integer comb[max][max];
     
+    static int combination(int n, int k) {
+        if (k > n-k) return combination(n, n-k);
+        if(r==0) return comb[n][r] = 1;
+        if(r==1) return comb[n][r] = n;
+        if(comb[n][r] != null) return comb[n][r]; 
+        return comb[n][r] = combination(n-1,r) + combination(n-1,r-1);
+    }
+    
     static int gcd(int a, int b) {
         if (b == 0)
             return a;
@@ -11,12 +19,17 @@ class Utils {
         return Math.abs(a * b) / gcdValue;
     } 
   
-    
-    static int combination(int n, int k) {
-        if (k > n-k) return combination(n, n-k);
-        if(r==0) return comb[n][r] = 1;
-        if(r==1) return comb[n][r] = n;
-        if(comb[n][r] != null) return comb[n][r]; 
-        return comb[n][r] = combination(n-1,r) + combination(n-1,r-1);
+    static List<Integer> primeFactor(int num) {
+        List<Integer> factors = new ArrayList();
+        for (int i = 2; i <= num/i; i++) {
+            while (num % i == 0) {
+                factors.add(i);
+                num /= i;
+            }
+        }
+        if (num > 1) 
+            factors.add(num);
+        return factors;
     }
+
 }
