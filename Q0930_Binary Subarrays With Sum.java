38669fa8af6df9760f91ee85c06b63f5fd601de4
Songyang Cheng <32532057+csy99@users.noreply.github.com>
2021-10-17 12:59:38 -0700

    Update Q0930_Binary Subarrays With Sum.java

--- Q0930_Binary Subarrays With Sum.java
+++ Q0930_Binary Subarrays With Sum.java
@@ -1,6 +1,7 @@
 /**
  * Created by csy99 on 5/29/20.
  */
+// hashmap, time: O(n), space: O(k)
 class Solution {
     public int numSubarraysWithSum(int[] A, int S) {
         int n = A.length;
@@ -14,9 +15,28 @@ class Solution {
         // looking for number i < j w/ sum[j]-sum[i] == S
         for (int i: sum) {
             res += count.getOrDefault(i, 0);
-            // System.out.printf("i:%d res:%d",i,res);
             count.put(i+S, count.getOrDefault(i+S, 0)+1);
-            // System.out.printf(" map[%d]=%d\n",i+S,count.get(i+S));
+        }
+        return res;
+    }
+}
+
+// two pointer, time: O(n), space: O(1)
+class Solution {
+    int n = 0;
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        n = nums.length;
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+    private int atMost(int[] nums, int goal) {
+        if (goal < 0) return 0;
+        int res = 0;
+        int i = 0;
+        for (int j = 0; j < n; j++) {
+            goal -= nums[j];
+            while (goal < 0)
+                goal += nums[i++];
+            res += j - i + 1;
         }
         return res;
     }
