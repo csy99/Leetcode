7c4fbe515d01d59ab3749ab47623c4dbf317bc1b
csy99 <32532057+csy99@users.noreply.github.com>
2022-11-27 11:23:09 -0800

    Update Q0446_Arithmetic_Slices_II_Subsequence.java

--- Q0446_Arithmetic_Slices_II_Subsequence.java
+++ Q0446_Arithmetic_Slices_II_Subsequence.java
@@ -1,31 +1,30 @@
-package Leetcode;
-
 /**
  * Created by csy99 on 3/24/21.
  */
-public class Q446_Arithmetic_Slices_II_Subsequence {
-    public int numberOfArithmeticSlices(int[] A) {
-        int n = A.length;
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int res = 0;
+        int n = nums.length;
         int[][] dp = new int[n][n];
         Map<Long, List<Integer>> map = new HashMap();
         for (int i = 0; i < n; i++) {
-            List<Integer> pos = map.getOrDefault((long)A[i], new ArrayList());
+            List<Integer> pos = map.getOrDefault((long) nums[i], new ArrayList());
             pos.add(i);
-            map.put((long)A[i], pos);
+            map.put((long) nums[i], pos);
         }
-        int res = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                long target = 2*(long)A[j] - A[i];  // the diff can be negative
-                if (map.containsKey(target)) {
-                    for (int k: map.get(target)) {
-                        if (k < j) 
-                            dp[i][j] += dp[j][k]+1;
-                        else
-                            break;
-                    }
+        
+        // i < j < k
+        for (int k = 0; k < n; k++) {
+            for (int j = 0; j < k; j++) {
+                long target = (long)2*nums[j] - nums[k];  // has to be long due to int overflow
+                List<Integer> pos = map.getOrDefault(target, new ArrayList());
+                for (int i: pos) {
+                    if (i < j)
+                        dp[j][k] += dp[i][j] + 1;
+                    else
+                        break;
                 }
-                res += dp[i][j];
+                res += dp[j][k];
             }
         }
         return res;
